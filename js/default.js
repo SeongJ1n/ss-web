042f7e554aae24aad13a05d006510ef030354573
seongj1n <happy8721@naver.com>
2016-07-28 14:59:42 +0900

    section-about 1차 완료

--- js/default.js
+++ js/default.js
@@ -1,3 +1,4 @@
+/* resizeSection */
 function resizeSection(){
   var winWidth = $(window).width();
   var winHeight = $(window).height();
@@ -23,16 +24,36 @@ $(window).on('resize', function(){
   resizeSection();
 });
 
+
+/* nav-scroll */
 $(window).scroll(function() {
   var winWidth = $(window).width();
   var winHeight = $(window).height();
   var $nav = $('#nav');
     if(winWidth >= 768){
-      if ($nav.offset().top > winHeight) {
+      if ( ($nav.offset().top)+100 > winHeight) {
           $nav.addClass("nav-scroll");
       }
       else {
           $nav.removeClass("nav-scroll");
       }
     }
+});
+
+/* sectionAbout animation */
+$(window).scroll(function() {
+  var winWidth = $(window).width();
+  var winHeight = $(window).height();
+  var $sectionAbout = $('#sectionAbout');
+  var $iphoneImg = $('#iphoneImg');
+  var $iphoneShadow = $('#iphoneShadow');
+  var $nav = $('#nav');
+  if( ($nav.offset().top)+200 > $sectionAbout.offset().top){
+    $iphoneImg.css('top', -185);
+    $iphoneShadow.css('opacity', 1);
+  }
+  else {
+    $iphoneImg.css('top', -300);
+    $iphoneShadow.css('opacity', 0.1);
+  }
 });
